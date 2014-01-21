64d0d774cb72d6b4a40e6dbbd56dc8ef45e22d2e
Jason Harley <jharley@redmind.ca>
2014-01-21 09:37:46 -0500

    switching from rvm to chruby

---
+++ system/_bash/400_chruby.sh
@@ -0,0 +1,14 @@
+if [[ -e /usr/local/share/chruby ]]; then
+  # Load chruby
+  source '/usr/local/share/chruby/chruby.sh'
+
+  # Automatically switch rubies
+  source '/usr/local/share/chruby/auto.sh'
+
+  # Set a default ruby if a .ruby-version file exists in the home dir
+  if [[ -f ~/.ruby-version ]]; then
+    chruby $(cat ~/.ruby-version)
+  else
+    echo "chruby: no default ruby set, using system ruby"
+  fi
+fi
--- system/_bash/499_rvm.sh
+++
@@ -1,2 +0,0 @@
-# Bring RVM into the environment, if we find it
-[ -d ${HOME}/.rvm ] && source ${HOME}/.rvm/scripts/rvm
