e99602568b4efa9299e35e35403577e4fe20e1d0
Jason Harley <jharley@loyalty.com>
2015-11-03 11:18:12 -0500

    Fixes for a Cygwin environments
    Finally doing proper guards for OS X specific tests.

--- system/_bash/010_python.sh
+++ system/_bash/010_python.sh
@@ -1,7 +1,9 @@
-if which brew 2>&1 > /dev/null; then
-  PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
+if [[ `uname` == 'Darwin' ]]; then
+    if [[ -e $(which brew 2>/dev/null) ]]; then
+        PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
 
-  if [ -d ${PYTHONPATH} ]; then
-    export PYTHONPATH
-  fi
+        if [ -d ${PYTHONPATH} ]; then
+            export PYTHONPATH
+        fi
+    fi
 fi
--- system/_bash/075_bash_completion.sh
+++ system/_bash/075_bash_completion.sh
@@ -1,9 +1,11 @@
-if which brew 2>&1 > /dev/null; then
-    BASHCOMPLETE_DIR="$(brew --prefix)/etc/bash_completion.d"
+if [[ `uname` == 'Darwin' ]]; then
+    if [[ -e $(which brew 2>/dev/null) ]]; then
+        BASHCOMPLETE_DIR="$(brew --prefix)/etc/bash_completion.d"
 
-    if [ -d $BASHCOMPLETE_DIR ]; then
-         for f in $BASHCOMPLETE_DIR/*; do
-            source $f
-        done
+        if [ -d $BASHCOMPLETE_DIR ]; then
+             for f in $BASHCOMPLETE_DIR/*; do
+                source $f
+            done
+        fi
     fi
 fi
