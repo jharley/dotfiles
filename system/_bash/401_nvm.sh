53ab697cb1ecdb809437143b01ae2905899a81f0
Jason Harley <jharley@redmind.ca>
2015-11-03 11:26:18 -0500

    NVM: untrack NVM_DIR, and make if it missing

--- system/_bash/401_nvm.sh
+++ system/_bash/401_nvm.sh
@@ -1,6 +1,9 @@
 if [[ `uname` == 'Darwin' ]]; then
     if [[ -d $(brew --prefix nvm 2>/dev/null) ]]; then
         export NVM_DIR=~/.nvm
+        if [ ! -d ${NVM_DIR} ]; then
+            mkdir -p ${NVM_DIR}
+        fi
 
         source $(brew --prefix nvm)/nvm.sh
 
