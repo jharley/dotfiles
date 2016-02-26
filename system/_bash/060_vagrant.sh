f2401ef34e1934e0064c8020c934e42fcc2be225
Jason Harley <jharley@redmind.ca>
2016-02-26 11:03:19 -0500

    Only setup Vagrant Fusion plugin if it's installed

--- system/_bash/060_vagrant.sh
+++ system/_bash/060_vagrant.sh
@@ -1,4 +1,14 @@
-if [ `uname` == 'Darwin' ]; then
+# Determine's if the Vagrant VMWare Fusion plugin is installed, and enables it
+# by default if it is available
+FUSION_PLUGIN_INSTALLED=1
+
+which vagrant &>/dev/null
+if [[ $? -eq 0 ]]; then
+  vagrant plugin list | grep -q vagrant-vmware-fusion
+  FUSION_PLUGIN_INSTALLED=$?
+fi
+
+if [[ $FUSION_PLUGIN_INSTALLED -eq 0 ]]; then
     export VAGRANT_DEFAULT_PROVIDER=vmware_fusion
     export VAGRANT_VMWARE_CLONE_DIRECTORY=${HOME}/.vagrant.d/projects
 
