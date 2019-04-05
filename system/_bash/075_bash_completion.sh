308af8c7d487c7d924f98e7ad0d75cbf65d5020a
Jason Harley <jharley@workmarket.com>
2019-04-05 13:53:32 -0400

    fix Brew-managed bash completion

--- system/_bash/075_bash_completion.sh
+++ system/_bash/075_bash_completion.sh
@@ -1,12 +1,10 @@
 if [[ `uname` == 'Darwin' ]]; then
     if [[ -e $(which brew 2>/dev/null) ]]; then
-        BASHCOMPLETE_DIR="$(brew --prefix)/etc/bash_completion.d"
+        BASHCOMPLETE_DIR="$(brew --prefix)/etc/profile.d"
 
-        if [ -d $BASHCOMPLETE_DIR ]; then
-             for f in $BASHCOMPLETE_DIR/*; do
-                source $f
-            done
-        fi
+       if [[ -r "$BASHCOMPLETE_DIR/bash_completion.sh" ]]; then
+         . "$BASHCOMPLETE_DIR/bash_completion.sh"
+       fi
     fi
 
     if [[ -e $(which aws 2>/dev/null) ]]; then
