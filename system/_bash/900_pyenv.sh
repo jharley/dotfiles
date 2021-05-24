a38a2d54d308de05e76fb59238f217e6802b58a9
Jason Harley <jharley@redmind.ca>
2021-05-24 10:25:01 -0400

    pyenv: upgrade init syntax for 2.x

--- system/_bash/900_pyenv.sh
+++ system/_bash/900_pyenv.sh
@@ -1,5 +1,7 @@
 if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init --path)"
 fi
 
 if command -v pyenv-virtualenv 1>/dev/null 2>&1; then
