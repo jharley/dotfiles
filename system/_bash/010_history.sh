5033c62f8eb25b60990a2751ebada6b639288ff0
Jason Harley <jharley@redmind.ca>
2021-06-07 15:59:03 -0400

    bash - timestamp bash history

--- system/_bash/010_history.sh
+++ system/_bash/010_history.sh
@@ -1,3 +1,4 @@
 export HISTSIZE=10000 # Store 10,000 history entries
 export HISTCONTROL=erasedups # Don't store duplicates
+export HISTTIMEFORMAT='%F %T '
 shopt -s histappend # Append to history file
