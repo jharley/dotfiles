d9d33847380473f17e9af0c5936a47d8ced8fd31
Jason Harley <jharley@redmind.ca>
2025-11-24 11:12:11 -0500

    asdf refactor

--- system/_bash/100_asdf.sh
+++ system/_bash/100_asdf.sh
@@ -1,20 +1,21 @@
-ASDF_PLUGINS="golang python java maven nodejs terraform ruby"
+export ASDF_DATA_DIR="${HOME}/.asdf"
 
+ASDF_PLUGINS="golang python java maven nodejs terraform ruby"
 if [[ $(uname) == 'Darwin' ]]; then
     if [[ -e $(which asdf 2>/dev/null) ]]; then
-	. "$(brew --prefix asdf)/libexec/asdf.sh"
-
         INSTALLED_PLUGINS=$(asdf plugin list)
         for plugin in $ASDF_PLUGINS; do
           if ! echo "$INSTALLED_PLUGINS" | grep -qw "$plugin" 2>/dev/null 1>&2; then
             asdf plugin add "$plugin"
           fi
         done
+
+        . <(asdf completion bash)
     fi
 fi
 
 if [ -f ~/.asdf/plugins/golang/set-env.bash ]; then
-  . ~/.asdf/plugins/golang/set-env.bash
+  . ${ASDF_DATA_DIR}/plugins/golang/set-env.bash
   export ASDF_GOLANG_MOD_VERSION_ENABLED=false
 
   # run the env adjustment function each prompt invocation
@@ -23,6 +24,8 @@ if [ -f ~/.asdf/plugins/golang/set-env.bash ]; then
   fi
 fi
 
-if [ -f ~/.asdf/plugins/java/set-java-home.bash ]; then
-  . ~/.asdf/plugins/java/set-java-home.bash
+if [ -f ${ASDF_DATA_DIR}/plugins/java/set-java-home.bash ]; then
+  . ${ASDF_DATA_DIR}/plugins/java/set-java-home.bash
 fi
+
+export PATH="${ASDF_DATA_DIR}/shims:$PATH"
